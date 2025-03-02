204320208d48f59d003f720e62ef7ec2291e866a
Thang Loi Tran <tranthangloi.dev@gmail.com>
2025-03-03 00:45:27 +0700

    fix: force scheme https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (!app()->environment('local')) {
-            $this->app['request']->server->set('HTTPS', true);
+        if(!$this->app->environment('local')) {
+            URL::forceScheme('https');
         }
     }
 }
