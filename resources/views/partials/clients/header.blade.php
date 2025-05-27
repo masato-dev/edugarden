76d1809f50bf13438aa8510fb729ce9c4c6cd439
Tran Thang Loi <thangloitran406@gmail.com>
2025-05-27 18:07:30 +0700

    feat: menu management

--- resources/views/partials/clients/header.blade.php
+++ resources/views/partials/clients/header.blade.php
@@ -13,10 +13,7 @@
                             <span>Tên sách</span>
                         </div>
                         <div class="col-9" id="headerSearchInputWrapper">
-                            <input 
-                                value="{{ request('keyword') ?? '' }}"
-                                type="text" 
-                                id="headerSearchInput" 
+                            <input value="{{ request('keyword') ?? '' }}" type="text" id="headerSearchInput"
                                 placeholder="Nhập nội dung tìm kiếm...">
 
                             <ul class="list-unstyled d-none" id="headerSearchResult">
@@ -34,16 +31,18 @@
             <div class="col-4">
                 @auth('user:web')
                     <div class="d-flex align-items-center justify-content-end gap-3">
-                        <a href="{{ route('carts.index') }}" class="k-btn btn-text-color d-flex gap-2 text-decoration-none py-3" id="headerCartBtn">
+                        <a href="{{ route('carts.index') }}"
+                            class="k-btn btn-text-color d-flex gap-2 text-decoration-none py-3" id="headerCartBtn">
                             <i class="icon ic-shopping-cart"></i>
                             <span>Giỏ hàng</span>
                             <span class="header-cart-quantity @if($cartAmount == 0) d-none @endif" id="headerCartQuantity">
                                 {{ $cartAmount  }}
                             </span>
                         </a>
-    
+
                         <div class="dropdown">
-                            <button class="k-btn btn-main dropdown-toggle d-flex gap-2 py-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <button class="k-btn btn-main dropdown-toggle d-flex gap-2 py-3" type="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
                                 <i class="icon ic-register"></i>
                                 <span>{{ auth('user:web')->user()->name }}</span>
                             </button>
@@ -70,16 +69,17 @@
             </div>
         </div>
     </div>
-    
-    <nav class="navbar navbar-expand-lg bg-light border-top border-bottom">
-        <div class="container">
-            <a class="navbar-brand d-lg-none" href="/">Menu</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#responsiveNavbar" aria-controls="responsiveNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse justify-content-between" id="responsiveNavbar">
-                @if(!empty($menu) && !empty($menu->menuItems))
+    @if(!empty($menu) && !empty($menu->menuItems))
+        <nav class="navbar navbar-expand-lg bg-light border-top border-bottom">
+            <div class="container">
+                <a class="navbar-brand d-lg-none" href="/">Menu</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#responsiveNavbar"
+                    aria-controls="responsiveNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-between" id="responsiveNavbar">
                     <ul class="navbar-nav w-100 justify-content-between py-2">
                         @foreach ($menu->menuItems as $item)
                             @php
@@ -91,10 +91,9 @@
                             </a>
                         @endforeach
                     </ul>
-                @endif
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    @endif
 
 </header>
-
