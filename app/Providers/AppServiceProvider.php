ec0dd4e1d2fc6e465544490965ffb6343af6c125
Tran Thang Loi <thangloitran406@gmail.com>
2025-05-27 17:26:39 +0700

    fix: migration datatype

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use URL;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,7 +14,37 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        
+        URL::macro('alternateHasCorrectSignature',function (Request $request, $absolute = true, array $ignoreQuery = []) {
+        $ignoreQuery[] = 'signature';
+            $absoluteUrl = url($request->path());
+            $url = $absolute ? $absoluteUrl : '/' . $request->path();
+
+            $queryString = collect(explode('&', (string) $request
+                ->server->get('QUERY_STRING')))
+                ->reject(fn($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
+                ->join('&');
+
+            $original = rtrim($url . '?' . $queryString, '?');
+
+            // Use the application key as the HMAC key
+            $key = config('app.key'); // Ensure app.key is properly set in .env
+
+            if (empty($key)) {
+                throw new \RuntimeException('Application key is not set.');
+            }
+
+            $signature = hash_hmac('sha256', $original, $key);
+            return hash_equals($signature, (string) $request->query('signature', ''));
+        });
+
+        URL::macro('alternateHasValidSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+                && URL::signatureHasNotExpired($request);
+        });
+
+        Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+        });
     }
 
     /**
