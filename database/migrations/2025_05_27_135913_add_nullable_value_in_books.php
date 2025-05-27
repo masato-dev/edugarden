dbec9c0caf234399479aa8b3c936095ebf4f4504
Thang Loi Tran <tranthangloi.dev@gmail.com>
2025-05-27 20:59:39 +0700

    feat: book management

---
+++ database/migrations/2025_05_27_135913_add_nullable_value_in_books.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->float('rating')->default(5)->nullable()->change();
+            $table->integer('buy_quantity')->default(0)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
+    }
+};
