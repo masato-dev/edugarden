afa0d46a158c93728481c918853a01d4167fae17
Thang Loi Tran <tranthangloi.dev@gmail.com>
2025-06-03 23:15:47 +0700

    feat: change footer color

--- resources/views/partials/clients/footer.blade.php
+++ resources/views/partials/clients/footer.blade.php
@@ -2,29 +2,29 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-4">
-                <h5 class="text-main fw-600">Về chúng tôi</h5>
-                <p class="text-white">
+                <h5 class="text-white fw-600">Về chúng tôi</h5>
+                <p style="color: #BACFDB;">
                     Chúng tôi là công ty dược phẩm uy tín tại Việt Nam, luôn cam kết
                     mang lại cho khách hàng những sản phẩm chất lượng và dịch vụ
                     tốt nhất.
                 </p>
             </div>
             <div class="col-12 col-md-4">
-                <h5 class="text-main fw-600">Liên hệ</h5>
+                <h5 class="text-white fw-600">Liên hệ</h5>
                 <ul class="list-unstyled">
                     <li class="mb-2">
-                        <span class="text-white">Số 123, đường ABC, phường DEF, thành phố GHI</span>
+                        <span style="color: #BACFDB;">Số 123, đường ABC, phường DEF, thành phố GHI</span>
                     </li>
                     <li class="mb-2">
-                        <span class="text-white">0123 456 7890</span>
+                        <span style="color: #BACFDB;">0123 456 7890</span>
                     </li>
                     <li class="mb-2">
-                        <span class="text-white">support@domain.com</span>
+                        <span style="color: #BACFDB;">support@domain.com</span>
                     </li>
                 </ul>
             </div>
             <div class="col-12 col-md-4">
-                <h5 class="text-main fw-600">Liên kết nhanh</h5>
+                <h5 class="text-white fw-600">Liên kết nhanh</h5>
                 <ul class="list-unstyled quick-links">
                     <li class="mb-2">
                         <a href="#" class="text-decoration-none text-white">Trang chủ</a>
@@ -49,7 +49,7 @@
 
     <div class="text-center text-white mt-5">
         <p class="mb-0">
-            Copyright &copy; 2025 by <a href="#" target="_blank" class="text-white">Edugarden</a>. All rights reserved.
+            Copyright &copy; 2025 by <a href="#" target="_blank" style="color: #BACFDB;">Edugarden</a>. All rights reserved.
         </p>
     </div>
 </footer>
