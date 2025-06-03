fb2bd7e305fb07bab89ab5a55e365263e32667a6
Tran Thang Loi <thangloitran406@gmail.com>
2025-06-03 11:42:43 +0700

    feat: SEO pages module

--- app/Http/Controllers/Client/Page/PageController.php
+++ app/Http/Controllers/Client/Page/PageController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Client\Page;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Core\ClientController;
 use App\Implementations\Services\Page\IPageService;
+use App\Utils\StringUtil;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PageController extends ClientController
 {
@@ -20,6 +22,11 @@ class PageController extends ClientController
         if($page == null) {
             return abort(404);
         }
+        $this->setMetadata(
+            $page->title, 
+            Str::limit(StringUtil::removeScriptTags($page->content), 200),
+            $page->title,
+        );
         return $this->getView('page.detail', ['page' => $page]);
     }
 }
