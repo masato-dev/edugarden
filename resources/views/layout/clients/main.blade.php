83ffa79756a28fe36417e803919911d89757b5f7
Thang Loi Tran <tranthangloi.dev@gmail.com>
2025-06-23 01:35:28 +0700

    feat: google search console

--- resources/views/layout/clients/main.blade.php
+++ resources/views/layout/clients/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="PW_uZYORIGtCWFvKcmzfGX2h9Vk4mEIIanej2_lut9g" />
      {!! \Artesaos\SEOTools\Facades\SEOMeta::generate() !!}
      {!! \Artesaos\SEOTools\Facades\OpenGraph::generate() !!}
      {!! \Artesaos\SEOTools\Facades\JsonLd::generate() !!}
